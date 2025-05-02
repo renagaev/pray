f7e40908ea1875a97ce47f2c7936425bf4c3ce40
Нагаев Ренат Евгеньевич <r.nagaev@tbank.ru>
2025-05-02 21:40:06 +0300

    remove unused components

--- tg_frontend/src/navigation/routes.tsx
+++ tg_frontend/src/navigation/routes.tsx
@@ -1,9 +1,7 @@
 import type { ComponentType, JSX } from 'react';
 
 import { IndexPage } from '@/pages/IndexPage/IndexPage';
-import { InitDataPage } from '@/pages/InitDataPage.tsx';
-import { LaunchParamsPage } from '@/pages/LaunchParamsPage.tsx';
-import { ThemeParamsPage } from '@/pages/ThemeParamsPage.tsx';
+
 
 interface Route {
   path: string;
@@ -14,7 +12,4 @@ interface Route {
 
 export const routes: Route[] = [
   { path: '/', Component: IndexPage },
-  { path: '/init-data', Component: InitDataPage, title: 'Init Data' },
-  { path: '/theme-params', Component: ThemeParamsPage, title: 'Theme Params' },
-  { path: '/launch-params', Component: LaunchParamsPage, title: 'Launch Params' }
 ];
--- tg_frontend/src/pages/IndexPage/IndexPage.tsx
+++ tg_frontend/src/pages/IndexPage/IndexPage.tsx
@@ -1,16 +1,7 @@
-import {List} from '@telegram-apps/telegram-ui';
 import type {FC} from 'react';
-
-import {Page} from '@/components/Page.tsx';
 
 import RequestForm from "@/components/RequestForm/RequestForm.tsx";
 
 export const IndexPage: FC = () => {
-    return (
-        <Page back={false}>
-            <List>
-                <RequestForm></RequestForm>
-            </List>
-        </Page>
-    );
+    return (<RequestForm></RequestForm>);
 };

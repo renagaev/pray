c7008bad77065bdadcbadbc1c9183b97ef590e65
Нагаев Ренат Евгеньевич <r.nagaev@tbank.ru>
2025-05-04 22:20:28 +0300

    add migration

--- backend/WebApplication/Migrations/20250504191827_Add_Reaction_CountTable.Designer.cs
+++ backend/WebApplication/Migrations/20250504191827_Add_Reaction_CountTable.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApplication;
 
 #nullable disable
 
-namespace Domain.Migrations
+namespace WebApplication.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250504191827_Add_Reaction_CountTable")]
+    partial class Add_Reaction_CountTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
